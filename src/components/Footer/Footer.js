70c2fbcd1682c5cf600bd4aa6ee07d8f501e7de2
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-02-09 20:33:52 +0300

    Linked social media icons and add new media files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,23 @@ const Footer = () => {
   return (
     <footer>
       <div className="social-medias">
-        <NavLink to={`/`}>
+        <NavLink to={`https://www.instagram.com/kker______/?hl=pt`} target="_blank">
           <FaInstagram />
         </NavLink>
-        <NavLink to={`/`}>
+        <NavLink to={`https://www.facebook.com/K.KambungoLDA`} target="_blank">
           <FaFacebook />
         </NavLink>
-        <NavLink to={`/`}>
+        <NavLink
+          to={`javascript:void(0)`}
+          onClick={() => (
+              window.location = 
+              'mailto:kklda37estudosnarussia@outlook.com'
+            )
+          }
+        >
           <FaMailBulk />
         </NavLink>
-        <NavLink to={`/`}>
+        <NavLink to={`https://wa.me/+244924760365`} target="_blank">
           <FaWhatsapp />
         </NavLink>
       </div>
--- src/components/KkerSocialMedias/KkerSocialMedias.js
+++ src/components/KkerSocialMedias/KkerSocialMedias.js
@@ -1,20 +1,28 @@
 import { FaInstagram, FaMailBulk, FaFacebook, FaWhatsapp } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 function KkerSocialMedias () {
   return (
     <div className="social-component">
-        <a>
+        <NavLink to={`https://www.instagram.com/kker______/?hl=pt`} target="_blank">
           <FaInstagram />
-        </a>
-        <a>
+        </NavLink>
+        <NavLink to={`https://www.facebook.com/K.KambungoLDA`} target="_blank">
           <FaFacebook />
-        </a>
-        <a>
+        </NavLink>
+        <NavLink
+          to={`javascript:void(0)`}
+          onClick={() => (
+              window.location = 
+              'mailto:kklda37estudosnarussia@outlook.com'
+            )
+          }
+        >
           <FaMailBulk />
-        </a>
-        <a>
+        </NavLink>
+        <NavLink to={`https://wa.me/+244924760365`} target="_blank">
           <FaWhatsapp />
-        </a>
+        </NavLink>
     </div>
   )
 }
